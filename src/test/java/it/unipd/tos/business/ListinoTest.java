441ec4f310778ee2f61a51ebdb007d9d26b90e9e
AlexUbuntu <alessandro99-c@live.com>
2020-12-08 22:31:12 +0100

    close #5

--- src/test/java/it/unipd/tos/business/ListinoTest.java
+++ src/test/java/it/unipd/tos/business/ListinoTest.java
@@ -1,3 +1,7 @@
+////////////////////////////////////////////////////////////////////
+// [ALESSANDRO] [CANEL] [1204681]
+////////////////////////////////////////////////////////////////////
+
 package it.unipd.tos.business;
 
 import static org.junit.Assert.assertEquals;
@@ -27,14 +31,20 @@ public class ListinoTest {
         
         ordinazioni.add(new MenuItem("Aceto", items.bevanda, 2.00));
         ordinazioni.add(new MenuItem("Propano", items.bevanda, 5.00));
-        ordinazioni.add(new MenuItem("Puffo", items.gelato, 3.00));
+        ordinazioni.add(new MenuItem("PuffoA", items.gelato, 3.00));
+        ordinazioni.add(new MenuItem("PuffoB", items.gelato, 3.00));
+        ordinazioni.add(new MenuItem("PuffoC", items.gelato, 3.00));
+        ordinazioni.add(new MenuItem("PuffoD", items.gelato, 3.00));
+        ordinazioni.add(new MenuItem("PuffoD", items.gelato, 2.00));
+        ordinazioni.add(new MenuItem("PuffoE", items.gelato, 1.00));
         ordinazioni.add(new MenuItem("Pannacotta", items.budino, 4.00));
     }  
     
     
-    //vediamo se la somma è corretta
+    //vediamo se la somma è corretta, cambia leggermente per lo sconto al gelato meno costoso
     @Test
     public void testSomma() throws TakeAwayBillException {
-        assertEquals(14.00, listino.getOrderPrice(ordinazioni, user), 0.00);
+        assertEquals(25.50, listino.getOrderPrice(ordinazioni, user), 0.00);
     }
+
 }
